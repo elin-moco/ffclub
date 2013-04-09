9272a68c569acc6cd32d99cb6edf63971217ebd2
Eddie Lin <elin@mozilla.com>
2013-04-09 19:35:44 +0800

    Added infinite scroll.

--- static/js/event.js
+++ static/js/event.js
@@ -1,6 +1,8 @@
 
 $(document).ready(function(){
 
+    var timeSpan = $('span.time');
+    timeSpan.prettyDate();
     var lightbox = new saw.Lightbox('.eventWall');
     $('.eventPhotoLink').click(
         function(e) {
@@ -12,15 +14,26 @@ $(document).ready(function(){
     $('.sharePhotoLink').click(function() {
         popup.show();
     });
+    var eventPhotos = $('.eventPhotos');
+    eventPhotos.masonry({
+        'itemSelector': '.eventPhoto',
+        columnWidth: 320,
+        isAnimated: true
+    });
+    eventPhotos.infinitescroll({
+
+            navSelector  : "#page-nav",
+            // selector for the paged navigation (it will be hidden)
+            nextSelector : "#page-nav a",
+            // selector for the NEXT link (to page 2)
+            itemSelector : ".eventPhoto"
+            // selector for all items you'll retrieve
+        },
+        function(photos) {
+            eventPhotos.masonry('appended', $(photos), true);
+            timeSpan.prettyDate();
+        });
+
+
 });
 
-$('.eventWall').imagesLoaded(
-    function() {
-        $(this).masonry({
-            'itemSelector': '.eventPhoto',
-            columnWidth: 320,
-            isAnimated: true
-        });
-    }
-);
-$('span.time').prettyDate();
