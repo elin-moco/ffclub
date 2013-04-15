65d0a80d149ffb2c2140a15d77f1b63e93c8ec19
Eddie Lin <elin@mozilla.com>
2013-04-15 11:28:09 +0800

    Force fields required.

--- ffclub/person/forms.py
+++ ffclub/person/forms.py
@@ -1,8 +1,17 @@
-from django.forms import ModelForm
+# -*- coding: utf-8 -*-
+from django.forms import ModelForm, ValidationError
 from ffclub.person.models import Person
 
 
 class PersonForm(ModelForm):
+
+    def clean_gender(self):
+        gender = self.cleaned_data['gender']
+        if gender == 'unknown':
+            raise ValidationError('未指定性別')
+        else:
+            return gender
+
     class Meta:
         model = Person
         fields = ('fullname', 'gender', 'address', 'occupation')
