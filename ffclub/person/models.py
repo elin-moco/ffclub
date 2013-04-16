a420ec11035e91f6be2cc8499f33056024a43214
Eddie Lin <elin@mozilla.com>
2013-04-16 12:34:03 +0800

    Fixed register form labels, testing delete link.

--- ffclub/person/models.py
+++ ffclub/person/models.py
@@ -7,10 +7,10 @@ class Person(models.Model):
 
     user = models.OneToOneField(User, primary_key=True, unique=True)
     fullname = models.CharField(max_length=255, verbose_name='姓名(*)')
-    gender = models.CharField(max_length=7, verbose_name='性別',
+    gender = models.CharField(max_length=7, verbose_name='性別(*)',
                               choices=(('unknown', '未指定'), ('male', '男性'), ('female', '女性')), default='unknown')
-    address = models.CharField(max_length=255, verbose_name='地址')
-    occupation = models.CharField(max_length=255, verbose_name='職業')
+    address = models.CharField(max_length=255, verbose_name='地址(*)')
+    occupation = models.CharField(max_length=255, verbose_name='職業(*)')
     status = models.CharField(max_length=20,
                               choices=(('normal', '正常'), ('spam', '垃圾')),
                               default='normal')
