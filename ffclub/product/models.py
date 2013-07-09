0a9b99f12406fc5b95d973379e78f3341c49f4a0
Eddie Lin <elin@mozilla.com>
2013-07-09 17:49:26 +0800

    Added rule for sending geolocation range.

--- ffclub/product/models.py
+++ ffclub/product/models.py
@@ -37,7 +37,7 @@ class Order(models.Model):
     usage = models.TextField(max_length=512, verbose_name='預計推廣對象 以及 將如何使用 Firefox 活力宣傳物(*)',
                              help_text='申請審核通過後，我們會將宣傳品寄送給你，請確認以下收件人資料:')
     fullname = models.CharField(max_length=255, verbose_name='姓名(*)')
-    email = models.EmailField(verbose_name='電子郵件(*)')
+    email = models.EmailField(verbose_name='電子郵件(*)', help_text='目前僅提供台灣本島及外島寄送服務，國際郵件暫不受理。')
     address = models.CharField(max_length=255, verbose_name='寄送地址(*)')
     occupation = models.CharField(max_length=255, verbose_name='職業(*)')
     feedback = models.TextField(max_length=512, verbose_name='其它建議', blank=True, default='')
