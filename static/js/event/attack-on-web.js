c23449886549f7a24afb1777ffab9d5c6b77d4e2
Bingdow <bingdow@gmail.com>
2013-08-27 21:17:25 +0800

    v3, black and fixed navigator.

--- static/js/event/attack-on-web.js
+++ static/js/event/attack-on-web.js
@@ -1,7 +1,25 @@
+$(window).scroll(function(){
+	if ($(window).scrollTop()<=200){
+		if($('#masthead').hasClass('fixed')){
+			$('#masthead').removeClass('fixed');
+			if($('#tabzilla').hasClass('tabzilla-opened'))
+				$("html, body").animate({ scrollTop: 0 }, 600);
+    	}
+	}else{
+		if(!$('#masthead').hasClass('fixed'))
+			$('#masthead').addClass('fixed');
+	}
+	if(!$('#tabzilla').hasClass('tabzilla-opened')){
+		if ($(window).scrollTop()!=0)
+			if(!$('#masthead').hasClass('fixed'))
+				$('#masthead').addClass('fixed');
+	}
+	return false;
+});
 // scroll action buttom
-$("#menu-principal.menu li a").click(function() {
+$('#menu-principal.menu li a').click(function() {
 	$('body, html').stop();
 	//$('body, html').animate({ scrollTop: ($($(this).attr("href")).offset().top  }, "slow");
-	$('body, html').animate({ scrollTop: ($($(this).attr("href")).offset().top ) }, "slow");
+	$('body, html').animate({ scrollTop: ($($(this).attr("href")).offset().top - 70) }, "slow");
 	return false;
 });
