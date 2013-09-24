55a7da8ddd2f0147d4b03741d7c784ccef83b64c
Eddie Lin <elin@mozilla.com>
2013-09-24 12:42:18 +0800

    Minor style fix.

--- static/js/event/every-moment-wall.js
+++ static/js/event/every-moment-wall.js
@@ -1,16 +1,35 @@
 "use strict";
 
 
-(function() {
-    $('.eventPhoto').mouseover(function() {
+(function () {
+    $('.eventPhoto').mouseover(function () {
+        var url = $(this).find('a').attr('href');
         $('#fxos-phone-frame').show();
         $('#fxos-phone-frame').position({
             my: 'center',
             at: 'center',
             of: $(this),
-            using: function(css) {
+            using: function (css) {
                 $('#fxos-phone-frame').animate(css, 350);
             }
         });
+        $('.bigShareButton').attr('data-url', url);
+    });
+    $('.bigShareButton').click(function () {
+        FB.ui({
+                method: 'feed',
+                name: 'Facebook Dialogs',
+                link: $(this).attr('data-url'),
+                caption: 'Reference Documentation',
+                description: 'Dialogs provide a simple, consistent interface for applications to interface with users.'
+            },
+            function (response) {
+                if (response && response.post_id) {
+                    alert('Post was published.');
+                } else {
+                    alert('Post was not published.');
+                }
+            }
+        );
     });
 })();
